83923bed9bdac768a135fdbf87aa4e61cbac43c2
Uriel <21690078@tecvalles.mx>
2025-09-03 13:10:08 -0600

    Update Linear Regression: rename methods , add typing annotations, kgwars, and Axo decorators

--- classification_algorithms/linear_regression.py
+++ classification_algorithms/linear_regression.py
@@ -1,44 +1,62 @@
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import LinearRegression as LR
+from sklearn.metrics import mean_squared_error, r2_score, classification_report
 import matplotlib.pyplot as plt
 
 
-# from sklearn.datasets import make_regression
-# from sklearn.model_selection import train_test_split
+from sklearn.datasets import make_regression
+from sklearn.model_selection import train_test_split
+import numpy.typing as npt
+from typing import Dict, Any
+from axo import Axo, axo_method
 
-class LinearRegression:
-    def __init__(self, X_train, X_test, y_train, y_test, **kwargs):
+class LinearRegression(Axo):
+    def __init__(
+        self, 
+        X_train: npt.NDArray, 
+        X_test: npt.NDArray, 
+        y_train: npt.NDArray, 
+        y_test: npt.NDArray, 
+        **kwargs
+    ):
         self.X_train = X_train
         self.X_test = X_test
         self.y_train = y_train
         self.y_test = y_test
-        self.modelo = LinearRegression(**kwargs)
+        self.modelo = LR(**kwargs)
 
-    def entrenar(self):
-        self.modelo.fit(self.X_train, self.y_train)
+    @axo_method
+    def train(self, **kwargs) -> Dict[str, Any]:
+        fitted = self.modelo.fit(self.X_train, self.y_train)
+        return fitted.get_params()
 
-    def predecir(self):
+    @axo_method
+    def predict(self, **kwargs) -> npt.NDArray:
         return self.modelo.predict(self.X_test)
 
-    def metricas(self):
-        y_pred = self.predecir()
+    def get_metrics(self, **kwargs) -> Dict[str, Any]:
+        y_pred = self.predict()
         mse = mean_squared_error(self.y_test, y_pred)
         r2 = r2_score(self.y_test, y_pred)
-        print(f"MSE: {mse:.4f}")
-        print(f"R²: {r2:.4f}")
+        return {
+            "MSE": mse,
+            "R2": r2
+        }
 
-    def grafico_regresion(self):
-        y_pred = self.predecir()
+    def graph_regression(self)-> None:
+        y_pred = self.predict()
         plt.scatter(self.y_test, y_pred, alpha=0.6)
         plt.xlabel("Valores reales")
         plt.ylabel("Predicciones")
         plt.title("Regresión Lineal - Comparación Real vs Predicción")
         plt.show()
-# if __name__ == "__main__":
-#     X, y = make_regression(n_samples=200, n_features=5, noise=0.3, random_state=42)
-#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+        
+"""if __name__ == "__main__":
+    X, y = make_regression(n_samples=200, n_features=5, noise=0.3, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-#     modelo = LinearRegression(X_train, X_test, y_train, y_test)
-#     modelo.entrenar()
-#     modelo.metricas()
-#     modelo.grafico_regresion()
+    model  = LinearRegression(X_train, X_test, y_train, y_test)
+    model .train()
+    results = model.metrics()
+    print("MSE:", results["MSE"])
+    print("R²:", results["R2"])
+    model .graph_regression()"""
