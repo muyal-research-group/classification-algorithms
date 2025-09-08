2bccc1215c45db6ec0dd5b5537ee53568acc745c
Uriel <21690078@tecvalles.mx>
2025-09-08 11:52:48 -0600

    __init__:  Update import from MultiLayerPerceptron to MultiLayer

--- classification_algorithms/perceptron/__init__.py
+++ classification_algorithms/perceptron/__init__.py
@@ -1,2 +1,2 @@
-from classification_algorithms.perceptron.multi_layer import PerceptronMulticapa
+from classification_algorithms.perceptron.multi_layer import MultiLayer
 from classification_algorithms.perceptron.perceptron import Perceptron
